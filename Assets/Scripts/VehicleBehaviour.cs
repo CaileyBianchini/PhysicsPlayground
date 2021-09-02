b957682c1670d9a01a046086b4fb39bc4ca4ea29
Cailey <cailey.m.bianchini@gmail.com>
2021-09-02 17:01:17 -0500

    creating an obscicle
    made one part that makes player move block to continue

--- Assets/Scripts/VehicleBehaviour.cs
+++ Assets/Scripts/VehicleBehaviour.cs
@@ -4,68 +4,23 @@ using UnityEngine;
 
 public class VehicleBehaviour : MonoBehaviour
 {
-    public HingeJoint frontLeft;
-    public HingeJoint frontRight;
-    public HingeJoint backLeft;
-    public HingeJoint backRight;
+    private CharacterController _controller;
 
-    public int motorValue = 100;
+    private Vector3 _desiredVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
-        frontLeft = GetComponent<HingeJoint>();
-        frontRight = GetComponent<HingeJoint>();
-        backLeft = GetComponent<HingeJoint>();
-        backRight = GetComponent<HingeJoint>();
+        _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        //forward
-        if (Input.GetButtonDown("w"))
-        {
-            //both fronts
-            JointMotor motor1 = frontLeft.motor;
-            motor1.targetVelocity = motorValue;
-            motor1.force = motorValue;
-            JointMotor motor2 = frontRight.motor;
-            motor2.targetVelocity = motorValue;
-            motor2.force = motorValue;
-            frontLeft.motor = motor1;
-            frontRight.motor = motor2;
-        }
-        //backward
-        else if (Input.GetButtonDown("s"))
-        {
-            //both backs
-            JointMotor motor1 = backLeft.motor;
-            motor1.targetVelocity = motorValue;
-            motor1.force = motorValue;
-            JointMotor motor2 = backRight.motor;
-            motor2.targetVelocity = motorValue;
-            motor2.force = motorValue;
-            backLeft.motor = motor1;
-            backRight.motor = motor2;
-        }
-        //left
-        else if (Input.GetButtonDown("a"))
-        {
-            //front right
-            JointMotor motor = frontRight.motor;
-            motor.targetVelocity = motorValue;
-            motor.force = motorValue;
-            frontRight.motor = motor;
-        }
-        //right
-        else if (Input.GetButtonDown("d"))
-        {
-            //front left
-            JointMotor motor = frontLeft.motor;
-            motor.targetVelocity = motorValue;
-            motor.force = motorValue;
-            frontLeft.motor = motor;
-        }
+        //get movement input
+        _desiredVelocity.x = Input.GetAxis("Horizontal"); //_desiredVelocity.x is inputRight
+        _desiredVelocity.y = 0.0f;
+        _desiredVelocity.z = Input.GetAxis("Vertical");//_desiredVelocity.y is inputForward
+
     }
 }
