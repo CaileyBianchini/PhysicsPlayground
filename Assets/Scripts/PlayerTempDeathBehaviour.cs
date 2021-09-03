beff89aba347da3d6560d3d20a0b6d107ea8413c
Cailey <cailey.m.bianchini@gmail.com>
2021-09-03 14:53:38 -0500

    finished the car obstacle part

---
+++ Assets/Scripts/PlayerTempDeathBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerTempDeathBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private Animator _animator;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ragdoll"))
+        {
+            _animator.enabled = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ragdoll"))
+        {
+            _animator.enabled = true ;
+        }
+    }
+}
