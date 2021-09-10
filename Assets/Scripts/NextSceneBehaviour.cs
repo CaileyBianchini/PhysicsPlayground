f518d21b829665ac261973fd6e80722d5ea79ed7
Cailey <cailey.m.bianchini@gmail.com>
2021-09-10 13:57:54 -0500

    Created a script and added collision check and input check

---
+++ Assets/Scripts/NextSceneBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSceneBehaviour : MonoBehaviour
+{
+    public string nextSceneName;
+    public int scene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && Input.GetButtonDown("Next"))
+        {
+            //SceneManager.LoadScene(sceneName: nextSceneName);
+             SceneManager.LoadScene(sceneBuildIndex: scene);
+        }
+    }
+}
--- Assets/Scripts/Player/CharacterBehaviour.cs
+++ Assets/Scripts/Player/CharacterBehaviour.cs
@@ -31,37 +31,6 @@ public class CharacterBehaviour : MonoBehaviour
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    void SaveGame()
-    {
-        PlayerPrefs.SetInt("SavedInteger", intToSave);
-        PlayerPrefs.SetFloat("SavedFloat", floatToSave);
-        PlayerPrefs.SetString("SavedString", stringToSave);
-        PlayerPrefs.Save();
-        Debug.Log("Game data saved!");
-    }
-
-    void LoadGame()
-    {
-        if (PlayerPrefs.HasKey("SavedInteger"))
-        {
-            intToSave = PlayerPrefs.GetInt("SavedInteger");
-            floatToSave = PlayerPrefs.GetFloat("SavedFloat");
-            stringToSave = PlayerPrefs.GetString("SavedString");
-            Debug.Log("Game data loaded!");
-        }
-        else
-            Debug.LogError("There is no save data!");
-    }
-
-    void ResetData()
-    {
-        PlayerPrefs.DeleteAll();
-        intToSave = 0;
-        floatToSave = 0.0f;
-        stringToSave = "";
-        Debug.Log("Data reset complete");
-    }
-
     private void Update()
     {
 
@@ -139,7 +108,6 @@ public class CharacterBehaviour : MonoBehaviour
         //allows player to quite
         if (Input.GetButtonDown("Cancel"))
         {
-            SaveGame();
             Application.Quit();
         }
     }
